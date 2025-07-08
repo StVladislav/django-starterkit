6ea630bd02a64f1a08219e56539757b2d1233e89
sv6382 <sv6382@gmail.com>
2025-07-08 20:24:17 +0300

    Add some comments

--- src/_test/serializers.py
+++ src/_test/serializers.py
@@ -11,12 +11,15 @@ class ProductSerializer(serializers.ModelSerializer):
     P.S. If you want to create new products using category name not its ID - you will need to 
     re-write create and update methods.
     """
-    category = serializers.PrimaryKeyRelatedField(queryset=ProductCategory.objects.all())
-    category_name = serializers.CharField(source='category.name', read_only=True)
-    images = serializers.SerializerMethodField()
+    category = serializers.PrimaryKeyRelatedField(queryset=ProductCategory.objects.all()) # For returning ids
+    category_name = serializers.CharField(source='category.name', read_only=True) # Returning only names
+    images = serializers.SerializerMethodField() # Returning dict for each image from get_images method
     
-
-    def get_images(self, obj):
+    def get_images(self, obj) -> list[dict]:
+        """
+        This method related with 'images' field serilizer and returns 
+        list of dicts with url and is_main flag
+        """
         request = self.context.get('request')
         out = []
         for image_obj in obj.images.all():
@@ -30,7 +33,6 @@ class ProductSerializer(serializers.ModelSerializer):
                 })
         return out
     
-
     class Meta:
         model = Product
         fields = ("id", "name", "category", "category_name", "slug", "images")
